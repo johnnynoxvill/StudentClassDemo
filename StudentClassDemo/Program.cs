2453bb72609c9f3ecd7c9d6973b96e47ac63cf62
potatotail <trolison1@cnm.edu>
2021-06-22 23:38:26 -0600

    Initial Commit

---
+++ StudentClassDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StudentClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student molly = new Student();
+
+            //molly.Name = "Molly the Cat";
+            //Console.WriteLine(molly.Name);
+
+            Student.DoStuff();
+
+            Student joe = new Student();
+            joe.SetName("Joe Shmoe");
+
+            string joesName = joe.GetName();
+            Console.WriteLine("Joe's name is {0}", joesName);
+
+            Student jane = new Student();
+            jane.SetName("Jane");
+            Console.WriteLine("Jane's name is {0}", jane.GetName());
+
+            jane.SetScore1(100);
+            jane.SetScore2(50);
+            Console.WriteLine("Jane's average score is {0}", jane.GetAverageScore());
+
+            jane.SetScore2(90);
+            Console.WriteLine("Jane's new average score is {0}", jane.GetAverageScore());
+
+            Console.WriteLine("Jane's high score is {0}", jane.GetHighScore());
+
+            jane.SetScore2(110);
+            Console.WriteLine("Jane's new high score is {0}", jane.GetHighScore());
+
+        }
+    }
+}
---
+++ StudentClassDemo/Student.cs
@@ -0,0 +1,102 @@
+﻿using System;
+namespace StudentClassDemo
+{
+    public class Student
+    {
+    
+        private string name;
+        private int idNumber;
+        private double score1;
+        private double score2;
+        private double averageScore;
+
+        
+        public static void DoStuff()
+        {
+            Student molly = new Student();
+            molly.name = "Molly";
+            molly.idNumber = 42;
+            Console.WriteLine("Molly: {0}", molly.name);
+            Console.WriteLine("Molly: {0}", molly.idNumber);
+
+            Student someStudent = new Student();
+            Console.WriteLine("someStudent: {0}", someStudent.name);
+            Console.WriteLine("someStudent: {0}", someStudent.idNumber);
+
+
+        }
+
+        public Student()
+        {
+
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+
+        public int GetIdNumber()
+        {
+            return idNumber;
+        }
+
+        public void SetIdNumber(int newIdNumber)
+        {
+            idNumber = newIdNumber;
+        }
+
+        public double GetScore1()
+        {
+            return score1;
+        }
+
+        public void SetScore1(double newScore1)
+        {
+            score1 = newScore1;
+            UpdateAverageScore();
+        }
+
+        public double GetScore2()
+        {
+            return score2;
+        }
+
+        public void SetScore2(double newScore2)
+        {
+            score2 = newScore2;
+            UpdateAverageScore();
+        }
+
+        public double GetAverageScore()
+        {
+            return averageScore;
+        }
+
+        private void UpdateAverageScore()
+        {
+            averageScore = (score1 + score2) / 2;
+        }
+
+        public double GetHighScore()
+        {
+            if (score1 > score2)
+            {
+                return score1;
+            }
+            else
+            {
+                return score2;
+            }
+        }
+
+
+    }
+
+
+}
